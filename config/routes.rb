95407ff849a1089ef404a879e07769385f7c049f
onoumenon <tanhuitian@gmail.com>
2021-11-25 12:33:22 +0800

    create routes for otp generation and verification

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    post 'otp', to: 'otp#create'
+    get 'otp/verify', to: 'otp#verify'
+  end
 end
